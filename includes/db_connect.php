10eedefc25160b3229db08b799d673bf8f4ad479
thatdudetebo <164423262+thatdudetebo@users.noreply.github.com>
2025-06-25 05:31:39 +0200

    Update db_connect.php

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -4,6 +4,11 @@ $username = "root"; // Default XAMPP MySQL username
 $password = "";     // Default XAMPP MySQL password (empty)
 $dbname = "c2cshop_db";
 
+// $servername = "sql210.infinityfree.com";
+// $username = "if0_39248464"; // Default XAMPP MySQL username
+// $password = "Tz4RR5jKUT8ta";     // Default XAMPP MySQL password (empty)
+// $dbname = "if0_39248464_XXX";
+
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
